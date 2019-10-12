869d58fe8998e120829855b687116992680c4d1d
Pamela Montigny-Davis <pmontignydavis@cnm.edu>
2019-10-12 16:49:52 -0600

    made changes to erd model

---
+++ snap-10-11/erd.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8"/>
+	<title>Snap Challenge 10-11</title>
+</head>
+<body>
+<div style="text-align:center">
+	<h1>Entity Relationship Diagram</h1>
+</div>
+<img src="erd1.png">
+<div>
+</body>
+</html>
---
+++ snap-10-11/user-story.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8"/>
+	<title>Snap Challenge</title>
+</head>
+<body>
+<div style="text-align:center">
+	<h1>Snap Challenge 10-11</h1>
+</div>
+<h2>User Story</h2>
+<p>As a registered user of mailchimp, I can upload images to an image content block for professional looking website.</p>
+</body>
+</html>
